a664f59d1081ebeb2a502917fd6e84e2c44625ab
rathinreddy1402 <rathinreddy1402@gmail.com>
2023-08-24 08:11:34 +0530

    1

---
+++ MavenPratice7am/src/test/java/Pratice/Demo2Test.java
@@ -0,0 +1,21 @@
+package Pratice;
+
+import org.testng.annotations.Test;
+
+public class Demo2Test {
+
+	@Test
+	public void m2()
+	{
+		System.out.println("hello");
+	}
+}
+//pull back
+//1st pull
+//2nd push
+//2nd pull
+
+
+
+//push2
+
---
+++ MavenPratice7am/src/test/java/Pratice/DemoTest.java
@@ -0,0 +1,19 @@
+package Pratice;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public void m1()
+	{
+		//welcome
+		System.out.println("m1 running");
+
+		//bye
+		//pushBack
+//2nd push
+
+
+	}
+}
